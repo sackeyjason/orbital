f26eb51be6cf7497d82f8355a07b85ed5e9240a2
sackeyjason <planetearthjason@gmail.com>
2021-11-24 08:33:41 +0000

    3 files modified

--- src/input.js
+++ src/input.js
@@ -1,26 +1,26 @@
 const keyMap = {
-  37: "left",
-  38: "rotateR",
-  39: "right",
-  40: "down",
+  37: 'left',
+  38: 'rotateR',
+  39: 'right',
+  40: 'down',
 
-  90: "rotateL",
-  88: "rotateR",
-  32: "hardDrop",
+  90: 'rotateL',
+  88: 'rotateR',
+  32: 'hardDrop',
 
-  13: "enter",
+  13: 'enter',
 };
 
 const input = {
   init: (el, events) => {
-    el.addEventListener("keydown", (event) => {
+    el.addEventListener('keydown', (event) => {
       // console.log('event: ', event);
       const value = keyMap[event.keyCode];
-      if (value) events.push(["input", value]);
+      if (value) events.push(['input', value]);
     });
-    el.querySelectorAll("button").forEach((button) => {
-      button.addEventListener("touchstart", (event) => {
-        events.push(["input", event.target.dataset.input]);
+    el.querySelectorAll('button').forEach((button) => {
+      button.addEventListener('touchstart', (event) => {
+        events.push(['input', event.target.dataset.input]);
       });
     });
   },
